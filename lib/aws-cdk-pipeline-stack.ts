d9bcadc383cd70d63efd7ea4a8772d3f5db94dde
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2023-05-17 21:32:15 +0900

    cdk update

--- lib/aws-cdk-pipeline-stack.ts
+++ lib/aws-cdk-pipeline-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {CodePipeline, CodePipelineSource, CodeBuildStep} from 'aws-cdk-lib/pipelines'
-import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
-import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 import { MyPipelineAppStage } from './aws-cdk-pipeline-lambda-stage';
 import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
 import { AllowedMethods, Distribution, OriginAccessIdentity, PriceClass, SecurityPolicyProtocol, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
@@ -15,7 +13,7 @@ export class AwsCdkPipelineStack extends cdk.Stack {
 
 		// APIドキュメントのためのS3バケットを作成
 		const apiDocumentBucket = new Bucket(this, 'ApiDocumentBucket', {
-			bucketName: 'api-document-bucket',
+			bucketName: 'takiguchi-no-api-document-bucket',
 			publicReadAccess: false,
 			blockPublicAccess: BlockPublicAccess.BLOCK_ALL
 		})
@@ -38,36 +36,18 @@ export class AwsCdkPipelineStack extends cdk.Stack {
 		new CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
 
 		// Codepipelineを作成
-    const pipeline = new codepipeline.Pipeline(this, 'APIDocumentPipeline', {
-      pipelineName: 'APIDocumentPipeline',
-    });
-
-		const sourceOutput = new codepipeline.Artifact('SourceOutput');
-		const sourceAction = new codepipeline_actions.CodeStarConnectionsSourceAction({
-			actionName: 'Github',
-			owner: 'takiguchi-yu',
-			repo: 'aws-cdk-pipeline',
-			branch: 'main',
-			output: sourceOutput,
-			connectionArn: 'arn:aws:codestar-connections:ap-northeast-1:887277492962:connection/a6c5beb2-34a4-4224-99a9-0332ee4a054c',
+		const pipeline = new CodePipeline(this, "APIDocumentPipeline", {
+			pipelineName: "APIDocumentPipeline",
+			synth: new CodeBuildStep("Synth", {
+				input: CodePipelineSource.connection(
+					"takiguchi-yu/aws-cdk-pipeline",
+					"main", {
+						connectionArn: "arn:aws:codestar-connections:ap-northeast-1:887277492962:connection/a6c5beb2-34a4-4224-99a9-0332ee4a054c"
+					}),
+				installCommands: ["npm install -g aws-cdk"],
+				commands: ["npm ci", "npm run build", "npx cdk synth"],
+			})
 		});
-    pipeline.addStage({
-      stageName: 'Source',
-      actions: [sourceAction],
-    });
-
-		// const pipeline = new CodePipeline(this, "APIDocumentPipeline", {
-		// 	pipelineName: "APIDocumentPipeline",
-		// 	synth: new CodeBuildStep("Synth", {
-		// 		input: CodePipelineSource.connection(
-		// 			"takiguchi-yu/aws-cdk-pipeline",
-		// 			"main", {
-		// 				connectionArn: "arn:aws:codestar-connections:ap-northeast-1:887277492962:connection/a6c5beb2-34a4-4224-99a9-0332ee4a054c"
-		// 			}),
-		// 		installCommands: ["npm install -g aws-cdk"],
-		// 		commands: ["npm ci", "npm run build", "npx cdk synth"],
-		// 	})
-		// });
 
     // // ステージを追加
     // const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "Deploy", {}))
