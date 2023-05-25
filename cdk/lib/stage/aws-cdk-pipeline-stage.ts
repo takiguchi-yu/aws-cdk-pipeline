d8b762f4be3e8bdaf6d0b95366dca48fe1f1aa14
takiguchi-yu <takiguchi-yu@gnavi.co.jp>
2023-05-25 22:11:45 +0900

    BLEAからプロジェクト設定情報を引っ張ってきた

--- cdk/lib/stage/aws-cdk-pipeline-stage.ts
+++ cdk/lib/stage/aws-cdk-pipeline-stage.ts
@@ -26,7 +26,7 @@ export class AwsCdkPipelineStage extends Stage {
     // AWS CloudTrail configuration in Control Tower Landing Zone v3.0 will not create CloudWatch Logs LogGroup in each Guest Accounts.
     // And it will delete these LogGroups when AWS CloudTrial Configuration is disabled in case of updating Landing Zone version from older one.
     // BLEA should notify their alarms continuously. So, if there is no CloudTrail and CloudWatch Logs in Guest Account, BLEA creates them to notify the Alarms.
-    const logging = new Logging(stack, 'Logging');
+    // const logging = new Logging(stack, 'Logging');
 
     // Security Alarms
     // !!! Need to setup SecurityHub, GuardDuty manually on Organizations Management account
