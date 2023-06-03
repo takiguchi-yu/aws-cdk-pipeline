f25273abdb3d034b032b06ffdcd464e880eea119
takiguchi-yu <y.takiguti@gmail.com>
2023-06-03 23:05:38 +0900

    update

--- cdk/bin/aws-cdk-pipeline.ts
+++ cdk/bin/aws-cdk-pipeline.ts
@@ -6,17 +6,22 @@ import { devParameter, devPipelineParameter } from '../parameter';
 
 const app = new cdk.App();
 
-new AwsCdkPipelineStack(app, 'AwsCdkPipelineStack', {
-  env: {
-    account: devPipelineParameter.env.account || process.env.CDK_DEFAULT_ACCOUNT,
-    region: devPipelineParameter.env.region || process.env.CDK_DEFAULT_REGION,
-  },
-  tags: {
-    Repository: devPipelineParameter.sourceRepository,
-    Environment: devParameter.envName,
-  },
-  targetParameters: [devParameter],
-  sourceRepository: devPipelineParameter.sourceRepository,
-  sourceBranch: devPipelineParameter.sourceBranch,
-  sourceConnectionArn: devPipelineParameter.sourceConnectionArn,
-});
+// 開発環境
+if (process.env.CDK_DEFAULT_ACCOUNT == '887277492962') {
+  new AwsCdkPipelineStack(app, 'AwsCdkPipelineStack', {
+    env: {
+      account: devPipelineParameter.env.account || process.env.CDK_DEFAULT_ACCOUNT,
+      region: devPipelineParameter.env.region || process.env.CDK_DEFAULT_REGION,
+    },
+    tags: {
+      Repository: devPipelineParameter.sourceRepository,
+      Environment: devParameter.envName,
+    },
+    targetParameters: [devParameter],
+    sourceRepository: devPipelineParameter.sourceRepository,
+    sourceBranch: devPipelineParameter.sourceBranch,
+    sourceConnectionArn: devPipelineParameter.sourceConnectionArn,
+  });
+} else {
+  console.log('環境変数[CDK_DEFAULT_ACCOUNT]が設定されていません');
+}
