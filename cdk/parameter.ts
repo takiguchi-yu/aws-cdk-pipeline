0823e1ba2a56e11fa310925762db3529f7df0f2a
takiguchi-yu <y.takiguti@gmail.com>
2023-06-04 12:48:15 +0900

    cdk update

--- cdk/parameter.ts
+++ cdk/parameter.ts
@@ -4,8 +4,8 @@ export interface AppParameter {
   env?: Environment;
   envName: string;
   securityNotifyEmail: string;
-  securitySlackWorkspaceId?: string; // required if deploy via CLI
-  securitySlackChannelId?: string; // required if deploy via CLI
+  securitySlackWorkspaceId: string; // required if deploy via CLI
+  securitySlackChannelId: string; // required if deploy via CLI
 }
 
 export interface PipelineParameter {
@@ -30,32 +30,13 @@ export const devParameter: AppParameter = {
 
 // パイプラインパラメーター
 export const devPipelineParameter: PipelineParameter = {
-  env: { account: '887277492962', region: 'ap-northeast-1' },
+  env: {
+    account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
+  },
   envName: 'DevPipeline',
   sourceRepository: 'takiguchi-yu/aws-cdk-pipeline',
   sourceBranch: 'main',
-  sourceConnectionArn:
-    'arn:aws:codestar-connections:ap-northeast-1:887277492962:connection/a6c5beb2-34a4-4224-99a9-0332ee4a054c',
-};
-
-/**
- * テスト環境
- */
-
-// スタックパラメーター
-export const testParameter: AppParameter = {
-  envName: 'TEST',
-  securityNotifyEmail: 'notify-security@example.com',
-  securitySlackWorkspaceId: 'T8XXXXXXX',
-  securitySlackChannelId: 'C00XXXXXXXX',
-};
-
-// パイプラインパラメーター
-export const testPipelineParameter: PipelineParameter = {
-  env: { account: '887277492962', region: 'ap-northeast-1' },
-  envName: 'DevPipeline',
-  sourceRepository: 'takiguchi-yu/aws-cdk-pipeline',
-  sourceBranch: 'staging',
   sourceConnectionArn:
     'arn:aws:codestar-connections:ap-northeast-1:887277492962:connection/a6c5beb2-34a4-4224-99a9-0332ee4a054c',
 };
